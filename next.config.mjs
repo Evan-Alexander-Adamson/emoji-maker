f2a278fc821f32fd2bf6cf9f0691bc19fc6f9e7b
Evan Alexander Adamson <evan.alexander.adamson@gmail.com>
2025-01-12 21:59:14 -0600

    Configure for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/emoji-maker',
+  images: {
+    unoptimized: true,
+  },
   env: {
     REPLICATE_API_TOKEN: process.env.REPLICATE_API_TOKEN,
-  },
-  images: {
-    domains: ['replicate.delivery'],
   },
   webpack: (config) => {
     config.experiments = { ...config.experiments, topLevelAwait: true };
