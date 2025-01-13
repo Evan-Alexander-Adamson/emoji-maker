1358792ea477a883f4427004c686b0d3e0a1c5c4
Evan Alexander Adamson <evan.alexander.adamson@gmail.com>
2025-01-12 22:02:43 -0600

    Add image remote patterns configuration

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,16 @@ const nextConfig = {
   basePath: '/emoji-maker',
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'replicate.delivery',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      },
+    ],
   },
   env: {
     REPLICATE_API_TOKEN: process.env.REPLICATE_API_TOKEN,
